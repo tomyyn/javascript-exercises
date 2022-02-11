1b8eec864e677c0118c2bf945844e001047d9958
Tom <tomasnaiouf@gmail.com>
2022-02-10 21:23:37 -0300

    Complete first 4 excercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,del) {
+    args=Array.from(arguments);
+    for(let i = 1; i<args.length;i++) arr = arr.filter(val=>{return (!(val===args[i]))});
+    return arr;
 };
 
 // Do not edit below this line
