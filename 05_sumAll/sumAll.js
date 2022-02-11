2daf1d4cc93a379b788af286f0e5bc5395ab83c8
Tom <tomasnaiouf@gmail.com>
2022-02-10 21:33:45 -0300

    Complete fifth excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(((a<0)||(b<0))||
+    ((typeof a!= "number")||(typeof b != "number"))) return "ERROR";
 
+
+    if(a>b){
+        let aux;
+        aux=a;
+        a=b;
+        b=aux;
+    }
+    let sum=0;
+    for(let i=a; i<=b; i++) sum+=i;
+    return sum;
 };
 
 // Do not edit below this line
